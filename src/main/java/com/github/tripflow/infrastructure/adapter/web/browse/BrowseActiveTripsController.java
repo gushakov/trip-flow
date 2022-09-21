8c51000b5181dad44b5a67d44a17a90aaa344f12
gushakov <Georgy.Ushakov@unil.ch>
2022-09-21 14:25:52 +0200

    continue

--- src/main/java/com/github/tripflow/infrastructure/adapter/web/browse/BrowseActiveTripsController.java
+++ src/main/java/com/github/tripflow/infrastructure/adapter/web/browse/BrowseActiveTripsController.java
@@ -4,8 +4,10 @@ import com.github.tripflow.core.usecase.browse.BrowseActiveTripsInputPort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,6 +25,13 @@ public class BrowseActiveTripsController {
         BrowseActiveTripsInputPort useCase = applicationContext.getBean(BrowseActiveTripsInputPort.class);
 
         useCase.listTripsByActiveTasksAssignedToUser(newPik);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/continueTripTask",
+            consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    @ResponseBody
+    public void continueTripTask(){
+        log.debug("[POST] continueTripTask");
     }
 
 }
